3018e1c5874677e5c1b46d4f66811d9037e84033
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-07-25 11:21:52 +0300

    Return an array when submit the form button

--- script.js
+++ script.js
@@ -5,15 +5,8 @@ const addNewStep = document.querySelector('.add-new-step')
 const step = document.querySelector('.step');
 
 
-const handleSubmitBtn = event => {
-    event.preventDefault();
-
-    console.log(event.target);
-
-    form.reset();
-}
-
 const handleAddIngredientBtn = event => {
+    event.preventDefault();
     const myELement = `
         <input type="text" id="ingredient" required>
     `;
@@ -22,12 +15,40 @@ const handleAddIngredientBtn = event => {
 }
 
 const handleAddStepBtn = event => {
+    event.preventDefault();
     const myELement = `
         <input type="text" id="step" required>
     `;
     step.insertAdjacentHTML('beforeend', myELement)
-
 }
+
+const handleSubmitBtn = event => {
+    event.preventDefault();
+
+    const inputForm = event.target;
+    const {recipe, url, name, difficulty, timing} = inputForm;
+
+    const recipes = [
+        {
+            title: `${recipe.value}`,
+            picture: `${url.value}`,
+            author: `${name.value}`,
+            difficulty: `${difficulty.value}`,
+            timing: `${timing.value}`,
+            ingredients: ['eggs', 'salt', 'water'],
+            steps: [
+                'Put a pan on the fire',
+                'Crack the eggs on it',
+                'Wait, put them out',
+                'Add some salt on it',
+            ],
+        },
+    ];
+
+    console.log(recipes)
+    form.reset();
+}
+
 
 addNewStep.addEventListener('click', handleAddStepBtn)
 addNewIngredient.addEventListener('click', handleAddIngredientBtn)
